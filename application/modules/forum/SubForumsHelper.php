3871c56c34d2f0f2e73f4e5086d583f73eeae13c
Deep <deep@deep-station>
2015-07-01 04:43:17 +0400

    Main forum page, update DB

--- application/modules/forum/SubForumsHelper.php
+++ application/modules/forum/SubForumsHelper.php
@@ -39,10 +39,23 @@ class SubForumsHelper
                     sf.topics_count,
                     sf.posts_count,
                     sf.title,
-                    sf.description
+                    sf.description,
+
+                    fp.id            last_post_id,
+                    fp.authored_by   last_post_author_id,
+                    fp.last_modified last_post_modified,
+                    ft.title         last_topic_title,
+                    a.login          last_post_author_login
+
                 FROM forum_subforums sf
                 INNER JOIN forum_forums ff
                     ON ff.id = sf.forum_id
+                LEFT JOIN forum_posts fp
+                    ON fp.id = sf.last_post_id
+                LEFT JOIN forum_topics ft
+                    ON ft.id = fp.topic_id
+                LEFT JOIN members a
+                    ON a.id = fp.authored_by
                 {$filter}
                 ORDER BY ff.sort ASC, sf.sort ASC
             ",
