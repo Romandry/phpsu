f58bf75df5d5840633de13e0873d312119ebac53
Deep <deep@deep-station>
2015-07-09 02:18:24 +0400

    Fix tracker filters names 2

--- application/languages/ru/forumTrackerController.lang.php
+++ application/languages/ru/forumTrackerController.lang.php
@@ -7,13 +7,13 @@
 
 return array(
 
-    'forum_tracker_breadcrumbs_name'    => 'Трекер',
-    'forum_tracker_by_last_title'       => 'Последние сообщения',
-    'forum_tracker_by_posts_title'      => 'Топ кол-ва ответов',
-    'forum_tracker_by_unanswered_title' => 'Без ответов',
-    'forum_tracker_by_views_title'      => 'Топ кол-ва просмотров',
-    'forum_tracker_error'               => 'Ошибка',
-    'forum_tracker_page_not_found'      => 'Запрашиваемая страница навигации внутри трекера форума не существует',
-    'forum_tracker_request_invalid'     => 'Некорректные параметры запроса'
+    'forum_tracker_breadcrumbs_name' => 'Трекер',
+    'forum_tracker_by_last_title'    => 'Последние сообщения',
+    'forum_tracker_by_posts_title'   => 'Топ кол-ва ответов',
+    'forum_tracker_by_answers_title' => 'Без ответов',
+    'forum_tracker_by_views_title'   => 'Топ кол-ва просмотров',
+    'forum_tracker_error'            => 'Ошибка',
+    'forum_tracker_page_not_found'   => 'Запрашиваемая страница навигации внутри трекера форума не существует',
+    'forum_tracker_request_invalid'  => 'Некорректные параметры запроса'
 
 );
