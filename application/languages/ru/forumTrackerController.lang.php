5f476425051b9469e45aa41aee237906d80e3e70
Deep <deep@deep-station>
2015-07-08 23:22:36 +0400

    Fix tracker filters names

--- application/languages/ru/forumTrackerController.lang.php
+++ application/languages/ru/forumTrackerController.lang.php
@@ -9,9 +9,9 @@ return array(
 
     'forum_tracker_breadcrumbs_name'    => 'Трекер',
     'forum_tracker_by_last_title'       => 'Последние сообщения',
-    'forum_tracker_by_posts_title'      => 'По ответам',
+    'forum_tracker_by_posts_title'      => 'Топ кол-ва ответов',
     'forum_tracker_by_unanswered_title' => 'Без ответов',
-    'forum_tracker_by_views_title'      => 'По просмотрам',
+    'forum_tracker_by_views_title'      => 'Топ кол-ва просмотров',
     'forum_tracker_error'               => 'Ошибка',
     'forum_tracker_page_not_found'      => 'Запрашиваемая страница навигации внутри трекера форума не существует',
     'forum_tracker_request_invalid'     => 'Некорректные параметры запроса'
