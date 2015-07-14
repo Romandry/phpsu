ac126c69ebad9587a42546f7f6a6f91cefd8510f
agolovko <agolovko@comboapp.com>
2015-07-14 13:47:03 +0300

    add migrations

---
+++ misc/db/migrations/Version20150714134424.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20150714134424 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $sql = file_get_contents(__DIR__  . '/../../sql/install.mysql.sql');
+        $this->addSql($sql);
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
